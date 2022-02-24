8a7db0ddb5cff4c4c58502ae68c5e9f7bc37631f
Hidetatz Yaginuma <hidetatz@gmail.com>
2022-02-24 09:00:36 +0900

    add an example C code built for riscv

---
+++ examples/42.s
@@ -0,0 +1,18 @@
+	.file	"42.c"
+	.option nopic
+	.text
+	.align	1
+	.globl	main
+	.type	main, @function
+main:
+	addi	sp,sp,-16
+	sd	s0,8(sp)
+	addi	s0,sp,16
+	li	a5,42
+	mv	a0,a5
+	ld	s0,8(sp)
+	addi	sp,sp,16
+	jr	ra
+	.size	main, .-main
+	.ident	"GCC: (g5964b5cd727) 11.1.0"
+	.section	.note.GNU-stack,"",@progbits
