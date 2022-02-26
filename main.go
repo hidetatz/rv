5608f484624c761f298aacc898229470fd8001a9
Hidetatz Yaginuma <hidetatz@gmail.com>
2022-02-26 23:12:00 +0900

    decode

--- main.go
+++ main.go
@@ -56,9 +56,25 @@ func (cpu *CPU) Fetch(size uint8) uint64 {
 	return cpu.Bus.Read(cpu.PC, size)
 }
 
-// Exec executes the given 32-bit instruction.
-// TODO: support 16-bit instructions
-func (cpu *CPU) Exec(ins uint32) uint64 {
+// Instruction is a general-purpose 32-bit instruction representation.
+// This is to hold the decoded raw instruction binary string.
+// Some fields such as Rd, Funct3, Rs1... are called other name like imm or shamt in some instructions,
+// but the most common and widely used names are chosen.
+type Instruction struct {
+	Raw    uint32 // raw instruction
+	Opcode uint8  // 7-bit (from 0 to 6).
+	Rd     uint8  // 5-bit (from 7 to 11).
+	Funct3 uint8  // 3-bit (from 12 to 14).
+	Rs1    uint8  // 5-bit (from 15 to 19).
+	Rs2    uint8  // 5-bit (from 20 to 24).
+	Funct7 uint8  // 7-bit (from 25 to 31).
+}
+
+// Decode decodes the given 32-bit instruction.
+// Note that rd, funct3, rs1... does not always match the instruction format,
+// but they are just decoded by the location in the 32-bit.
+// With that consideration, the raw instruction binary is also stored in the returned struct.
+func (cpu *CPU) Decode(ins uint32) uint64 {
 	opcode := ins & 0x00_00_00_7f
 
 	switch opcode {
@@ -69,20 +85,30 @@ func (cpu *CPU) Exec(ins uint32) uint64 {
 		case 0b000:
 			funct7 := ins & 0xFE_00_00_00 >> 25
 			switch funct7 {
+			// add
 			case 0b000_0000:
+			// sub
 			case 0b010_0000:
 			}
+		// sll
 		case 0b001:
+		// slt
 		case 0b010:
+		// sltu
 		case 0b011:
+		// xor
 		case 0b100:
 		case 0b101:
 			funct7 := ins & 0xFE_00_00_00 >> 25
 			switch funct7 {
+			// srl
 			case 0b000_0000:
+			// sra
 			case 0b010_0000:
 			}
+		// or
 		case 0b110:
+		// and
 		case 0b111:
 		}
 	// I
