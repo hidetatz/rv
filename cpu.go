8d5ebeb32f35ea4bf70a00dd1b89fb993402fb03
Hidetatz Yaginuma <hidetatz@gmail.com>
2022-02-27 12:34:58 +0900

    implement Exec

--- cpu.go
+++ cpu.go
@@ -131,61 +131,43 @@ func (cpu *CPU) Decode32(inst uint32) *Instruction {
 }
 
 func (cpu *CPU) Exec(inst *Instruction) {
-	ins := &Instruction{
-		Raw:    inst,
-		Opcode: uint8(inst & 0x00_00_00_7f),
-		Rd:     uint8(inst & 0x00_00_0F_80 >> 7),
-		Funct3: uint8(inst & 0x00_00_70_00 >> 12),
-		Rs1:    uint8(inst & 0x00_0F_80_00 >> 15),
-		Rs2:    uint8(inst & 0x01_F0_00_00 >> 20),
-		Funct7: uint8(inst & 0xFE_00_00_00 >> 25),
-	}
-	// still fill Op
-
-	switch ins.Opcode {
-	case 0b011_0011:
-		switch ins.Funct3 {
-		case 0b000:
-			switch ins.Funct7 {
-			case 0b000_0000:
-				ins.Op = InsAdd
-			case 0b010_0000:
-				ins.Op = InsSub
-			}
-		case 0b001:
-			ins.Op = InsSLL
-		case 0b010:
-			ins.Op = InsSLT
-		case 0b011:
-			ins.Op = InsSLTU
-		case 0b100:
-			ins.Op = InsXOR
-		case 0b101:
-			switch ins.Funct7 {
-			case 0b000_0000:
-				ins.Op = InsSRL
-			case 0b010_0000:
-				ins.Op = InsSRA
-			}
-		case 0b110:
-			ins.Op = InsOr
-		case 0b111:
-			ins.Op = InsAnd
+	switch inst.Op {
+	case InsAdd:
+		// Arithmetic overflow should be ignored according to the RISC-V spec.
+		// In Go, primitive + ignores the overflow.
+		cpu.Regs.Write(inst.Rd, cpu.Regs.Read(inst.Rs1)+cpu.Regs.Read(inst.Rs2))
+	case InsSub:
+		cpu.Regs.Write(inst.Rd, cpu.Regs.Read(inst.Rs1)-cpu.Regs.Read(inst.Rs2))
+	case InsSLL:
+		// In RV64I, only the low 6 bits of rs2 are used as the shift amount
+		// This is the same as SRL and SRA
+		shift := cpu.Regs.Read(inst.Rs2) & 0b111111
+		cpu.Regs.Write(inst.Rd, cpu.Regs.Read(inst.Rs1)<<shift)
+	case InsSLT:
+		var v uint64 = 0
+		// must compare as two's complement
+		if int64(cpu.Regs.Read(inst.Rs1)) < int64(cpu.Regs.Read(inst.Rs2)) {
+			v = 1
 		}
-	case 0b110_0111, 0b000_0011, 0b001_0011, 0b000_1111, 0b111_0011:
-		fallthrough
-	case 0b010_0011:
-		fallthrough
-	case 0b110_0011:
-		fallthrough
-	case 0b011_0111, 0b001_0111:
-		fallthrough
-	case 0b110_1111:
-		fallthrough
-	default:
-		// TODO: define exception and return it
-		panic(fmt.Sprintf("unknown instruction: %b", ins))
+		cpu.Regs.Write(inst.Rd, v)
+	case InsSLTU:
+		var v uint64 = 0
+		// must compare as unsigned val
+		if cpu.Regs.Read(inst.Rs1) < cpu.Regs.Read(inst.Rs2) {
+			v = 1
+		}
+		cpu.Regs.Write(inst.Rd, v)
+	case InsXOR:
+		cpu.Regs.Write(inst.Rd, cpu.Regs.Read(inst.Rs1)^cpu.Regs.Read(inst.Rs2))
+	case InsSRL:
+		shift := cpu.Regs.Read(inst.Rs2) & 0b111111
+		cpu.Regs.Write(inst.Rd, cpu.Regs.Read(inst.Rs1)>>shift)
+	case InsSRA:
+		shift := cpu.Regs.Read(inst.Rs2) & 0b111111
+		cpu.Regs.Write(inst.Rd, uint64(int64(cpu.Regs.Read(inst.Rs1))>>shift))
+	case InsOr:
+		cpu.Regs.Write(inst.Rd, cpu.Regs.Read(inst.Rs1)|cpu.Regs.Read(inst.Rs2))
+	case InsAnd:
+		cpu.Regs.Write(inst.Rd, cpu.Regs.Read(inst.Rs1)&cpu.Regs.Read(inst.Rs2))
 	}
-
-	return ins
 }
