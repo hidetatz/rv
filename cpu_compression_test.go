ade12ac8309bfa446f66721a43d3f61de6b0a09f
Hidetatz Yaginuma <hidetatz@gmail.com>
2022-03-21 23:13:30 +0900

    add some test

--- cpu_compression_test.go
+++ cpu_compression_test.go
@@ -9,10 +9,26 @@ func TestCPU_Decompress(t *testing.T) {
 		expectedExcp Exception
 	}{
 		"c.sub": {
-			compressed:   0b100011_101_00_111_01,
-			expected:     0b0100000_01111_01101_000_01101_0110011,
-			expectedExcp: ExcpNone,
+			compressed: 0b1000_11101_00111_01,
+			expected:   0b0100000_01111_01101_000_01101_0110011,
 		},
+		"c.xor": {
+			compressed: 0b1000_11101_01111_01,
+			expected:   0b0000000_01111_01101_100_01101_0110011,
+		},
+		"c.or": {
+			compressed: 0b1000_11101_10111_01,
+			expected:   0b0000000_01111_01101_110_01101_0110011,
+		},
+		"c.and": {
+			compressed: 0b1000_11101_11111_01,
+			expected:   0b0000000_01111_01101_111_01101_0110011,
+		},
+		"c.mv": {
+			compressed: 0b1000_01101_01111_10,
+			expected:   0b0000000_01111_00000_000_01101_0110011,
+		},
+		//"c.add": {},
 	}
 
 	for name, tc := range tests {
