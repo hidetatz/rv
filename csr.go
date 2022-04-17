ba893a18dd339cf6ff155d39c4a420f0547fd547
Hidetatz Yaginuma <hidetatz@gmail.com>
2022-04-17 20:55:10 +0900

    fix signature

--- csr.go
+++ csr.go
@@ -8,59 +8,59 @@ const (
 
 	// Listed CSRs. See privileged architecture spec 2.2.
 	// U
-	CsrUSTATUS  = 0x000 // User status register.
-	CsrUIE      = 0x004 // User interrupt-enable register.
-	CsrUTVEC    = 0x005 // User trap handler base address.
-	CsrUSCRATCH = 0x040 // Scratch register for user trap handlers.
-	CsrUEPC     = 0x041 // User exception program counter.
-	CsrUCAUSE   = 0x042 // User trap cause.
-	CsrUTVAL    = 0x043 // User bad address or instruction.
-	CsrUIP      = 0x044 // User interrupt pending.
-	CsrFFLAGS   = 0x001 // Floating-Point Accrued Exceptions.
-	CsrFRM      = 0x002 // Floating-Point Dynamic Rounding Mode.
-	CsrFCSR     = 0x003 // Floating-Point Control and Status Register (frm + fflags).
-	CsrCYCLE    = 0xc00 // Read only. Cycle counter for RDCYCLE instruction.
-	CsrTIME     = 0xc01 // Read only. Timer for RDTIME instruction.
-	CsrINSTRET  = 0xc02 // Read only. Instructions-retired counter for RDINSTRET instruction.
-	CsrCYCLEH   = 0xc80 // Read only. Upper 32 bits of cycle, RV32I only.
-	CsrTIMEH    = 0xc81 // Read only. Upper 32 bits of time, RV32I only.
-	CsrINSTRETH = 0xc82 // Read only. Upper 32 bits of instret, RV32I only.
+	CsrUSTATUS  uint64 = 0x000 // User status register.
+	CsrUIE      uint64 = 0x004 // User interrupt-enable register.
+	CsrUTVEC    uint64 = 0x005 // User trap handler base address.
+	CsrUSCRATCH uint64 = 0x040 // Scratch register for user trap handlers.
+	CsrUEPC     uint64 = 0x041 // User exception program counter.
+	CsrUCAUSE   uint64 = 0x042 // User trap cause.
+	CsrUTVAL    uint64 = 0x043 // User bad address or instruction.
+	CsrUIP      uint64 = 0x044 // User interrupt pending.
+	CsrFFLAGS   uint64 = 0x001 // Floating-Point Accrued Exceptions.
+	CsrFRM      uint64 = 0x002 // Floating-Point Dynamic Rounding Mode.
+	CsrFCSR     uint64 = 0x003 // Floating-Point Control and Status Register (frm + fflags).
+	CsrCYCLE    uint64 = 0xc00 // Read only. Cycle counter for RDCYCLE instruction.
+	CsrTIME     uint64 = 0xc01 // Read only. Timer for RDTIME instruction.
+	CsrINSTRET  uint64 = 0xc02 // Read only. Instructions-retired counter for RDINSTRET instruction.
+	CsrCYCLEH   uint64 = 0xc80 // Read only. Upper 32 bits of cycle, RV32I only.
+	CsrTIMEH    uint64 = 0xc81 // Read only. Upper 32 bits of time, RV32I only.
+	CsrINSTRETH uint64 = 0xc82 // Read only. Upper 32 bits of instret, RV32I only.
 
 	// S
-	CsrSSTATUS    = 0x100 // Supervisor status register.
-	CsrSEDELEG    = 0x102 // Supervisor exception delegation register.
-	CsrSIDELEG    = 0x103 // Supervisor interrupt delegation register.
-	CsrSIE        = 0x104 // Supervisor interrupt-enable register.
-	CsrSTVEC      = 0x105 // Supervisor trap handler base address.
-	CsrSCOUNTEREN = 0x106 // Supervisor counter enable.
-	CsrSSCRATCH   = 0x140 // Scratch register for supervisor trap handlers.
-	CsrSEPC       = 0x141 // Supervisor exception program counter.
-	CsrSCAUSE     = 0x142 // Supervisor trap cause.
-	CsrSTVAL      = 0x143 // Supervisor bad address or instruction.
-	CsrSIP        = 0x144 // Supervisor interrupt pending.
-	CsrSATP       = 0x180 // Supervisor address translation and protection.
+	CsrSSTATUS    uint64 = 0x100 // Supervisor status register.
+	CsrSEDELEG    uint64 = 0x102 // Supervisor exception delegation register.
+	CsrSIDELEG    uint64 = 0x103 // Supervisor interrupt delegation register.
+	CsrSIE        uint64 = 0x104 // Supervisor interrupt-enable register.
+	CsrSTVEC      uint64 = 0x105 // Supervisor trap handler base address.
+	CsrSCOUNTEREN uint64 = 0x106 // Supervisor counter enable.
+	CsrSSCRATCH   uint64 = 0x140 // Scratch register for supervisor trap handlers.
+	CsrSEPC       uint64 = 0x141 // Supervisor exception program counter.
+	CsrSCAUSE     uint64 = 0x142 // Supervisor trap cause.
+	CsrSTVAL      uint64 = 0x143 // Supervisor bad address or instruction.
+	CsrSIP        uint64 = 0x144 // Supervisor interrupt pending.
+	CsrSATP       uint64 = 0x180 // Supervisor address translation and protection.
 
 	// M
-	CsrMVENDORID  = 0xf11 // Read only. Vendor ID.
-	CsrMARCHID    = 0xf12 // Read only. Architecture ID.
-	CsrMIMPID     = 0xf13 // Read only. Implementation ID.
-	CsrMHARTID    = 0xf14 // Read only. Hardware thread ID.
-	CsrMSTATUS    = 0x300 // Machine status register.
-	CsrMISA       = 0x301 // ISA and extensions
-	CsrMEDELEG    = 0x302 // Machine exception delegation register.
-	CsrMIDELEG    = 0x303 // Machine interrupt delegation register.
-	CsrMIE        = 0x304 // Machine interrupt-enable register.
-	CsrMTVEC      = 0x305 // Machine trap-handler base address.
-	CsrMCOUNTEREN = 0x306 // Machine counter enable.
-	CsrMSCRATCH   = 0x340 // Scratch register for machine trap handlers.
-	CsrMEPC       = 0x341 // Machine exception program counter.
-	CsrMCAUSE     = 0x342 // Machine trap cause.
-	CsrMTVAL      = 0x343 // Machine bad address or instruction.
-	CsrMIP        = 0x344 // Machine interrupt pending.
-	CsrPMPCFG0    = 0x3a0 // Physical memory protection configuration.
-	CsrPMPCFG1    = 0x3a1 // Physical memory protection configuration, RV32 only.
-	CsrPMPCFG2    = 0x3a2 // Physical memory protection configuration.
-	CsrPMPCFG3    = 0x3a3 // Physical memory protection configuration, RV32 only.
+	CsrMVENDORID  uint64 = 0xf11 // Read only. Vendor ID.
+	CsrMARCHID    uint64 = 0xf12 // Read only. Architecture ID.
+	CsrMIMPID     uint64 = 0xf13 // Read only. Implementation ID.
+	CsrMHARTID    uint64 = 0xf14 // Read only. Hardware thread ID.
+	CsrMSTATUS    uint64 = 0x300 // Machine status register.
+	CsrMISA       uint64 = 0x301 // ISA and extensions
+	CsrMEDELEG    uint64 = 0x302 // Machine exception delegation register.
+	CsrMIDELEG    uint64 = 0x303 // Machine interrupt delegation register.
+	CsrMIE        uint64 = 0x304 // Machine interrupt-enable register.
+	CsrMTVEC      uint64 = 0x305 // Machine trap-handler base address.
+	CsrMCOUNTEREN uint64 = 0x306 // Machine counter enable.
+	CsrMSCRATCH   uint64 = 0x340 // Scratch register for machine trap handlers.
+	CsrMEPC       uint64 = 0x341 // Machine exception program counter.
+	CsrMCAUSE     uint64 = 0x342 // Machine trap cause.
+	CsrMTVAL      uint64 = 0x343 // Machine bad address or instruction.
+	CsrMIP        uint64 = 0x344 // Machine interrupt pending.
+	CsrPMPCFG0    uint64 = 0x3a0 // Physical memory protection configuration.
+	CsrPMPCFG1    uint64 = 0x3a1 // Physical memory protection configuration, RV32 only.
+	CsrPMPCFG2    uint64 = 0x3a2 // Physical memory protection configuration.
+	CsrPMPCFG3    uint64 = 0x3a3 // Physical memory protection configuration, RV32 only.
 
 	// Named SSTATUS fields.
 	CsrSstatusUie  = 0x0000_0000_0000_0001 // sstatus[0]
@@ -78,22 +78,22 @@ const (
 	CsrSstatusMask = CsrSstatusUie | CsrSstatusSie | CsrSstatusUpie | CsrSstatusSpie | CsrSstatusSpp | CsrSstatusFs | CsrSstatusXs | CsrSstatusSum | CsrSstatusMxr | CsrSstatusUxl | CsrSstatusSd
 
 	// Named SIP fields.
-	CsrSipUSIP = 0x0000_0000_0000_0001 // sip[0]
-	CsrSipSSIP = 0x0000_0000_0000_0002 // sip[1]
-	CsrSipUTIP = 0x0000_0000_0000_0010 // sip[4]
-	CsrSipSTIP = 0x0000_0000_0000_0020 // sip[5]
-	CsrSipUEIP = 0x0000_0000_0000_0100 // sip[8]
-	CsrSipSEIP = 0x0000_0000_0000_0200 // sip[9]
+	CsrSipUSIP uint64 = 0x0000_0000_0000_0001 // sip[0]
+	CsrSipSSIP uint64 = 0x0000_0000_0000_0002 // sip[1]
+	CsrSipUTIP uint64 = 0x0000_0000_0000_0010 // sip[4]
+	CsrSipSTIP uint64 = 0x0000_0000_0000_0020 // sip[5]
+	CsrSipUEIP uint64 = 0x0000_0000_0000_0100 // sip[8]
+	CsrSipSEIP uint64 = 0x0000_0000_0000_0200 // sip[9]
 	// CsrSipMask is the field location which SIP can access (= the access level is under the supervisor).
 	CsrSipMask = CsrSipUSIP | CsrSipSSIP | CsrSipUTIP | CsrSipSTIP | CsrSipUEIP | CsrSipSEIP
 
 	// Named SIE fields.
-	CsrSieUSIE = 0x0000_0000_0000_0001 // sie[0]
-	CsrSieSSIE = 0x0000_0000_0000_0002 // sie[1]
-	CsrSieUTIE = 0x0000_0000_0000_0010 // sie[4]
-	CsrSieSTIE = 0x0000_0000_0000_0020 // sie[5]
-	CsrSieUEIE = 0x0000_0000_0000_0100 // sie[8]
-	CsrSieSEIE = 0x0000_0000_0000_0200 // sie[9]
+	CsrSieUSIE uint64 = 0x0000_0000_0000_0001 // sie[0]
+	CsrSieSSIE uint64 = 0x0000_0000_0000_0002 // sie[1]
+	CsrSieUTIE uint64 = 0x0000_0000_0000_0010 // sie[4]
+	CsrSieSTIE uint64 = 0x0000_0000_0000_0020 // sie[5]
+	CsrSieUEIE uint64 = 0x0000_0000_0000_0100 // sie[8]
+	CsrSieSEIE uint64 = 0x0000_0000_0000_0200 // sie[9]
 	// CsrSieMask is the field location which SIE can access (= the access level is under the supervisor).
 	CsrSieMask = CsrSieUSIE | CsrSieSSIE | CsrSieUTIE | CsrSieSTIE | CsrSieUEIE | CsrSieSEIE
 )
@@ -104,7 +104,7 @@ func NewCSR() CSR {
 
 // Read reads CSR by the given address. CSR address is 12-bit.
 // This method does not validate the CPU mode. The validation should be the caller's responsibility.
-func (csr CSR) Read(addr uint16) uint64 {
+func (csr CSR) Read(addr uint64) uint64 {
 	// assuming addr is small enough, not checking the index
 
 	if addr == CsrFFLAGS {
@@ -136,7 +136,7 @@ func (csr CSR) Read(addr uint16) uint64 {
 
 // WriteCSR write the given value to the CSR.
 // This method does not validate the CPU mode. The validation should be the caller's responsibility.
-func (csr CSR) Write(addr uint16, value uint64) {
+func (csr CSR) Write(addr uint64, value uint64) {
 	if addr == CsrFFLAGS {
 		// FCSR consists of FRM (3-bit) + FFLAGS (5-bit)
 		csr[CsrFCSR] &= ^uint64(0x1f) // clear fcsr[4:0]
