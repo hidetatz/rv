7a74a8835cdc77f8fd889d180c928436db961960
Hidetatz Yaginuma <hidetatz@gmail.com>
2022-05-02 17:52:03 +0900

    refactor Exception to represent the code itself

--- exception.go
+++ exception.go
@@ -1,21 +1,26 @@
 package main
 
-type Exception uint8
+type Exception int
 
 const (
-	ExcpNone Exception = iota
-	ExcpInstructionAddressMisalighed
-	ExcpInstructionAccessFault
-	ExcpIllegalInstruction
-	ExcpBreakpoint
-	ExcpLoadAddressMisaligned
-	ExcpLoadAccessFault
-	ExcpStoreAMOAddressMisaligned
-	ExcpStoreAMOAccessFault
-	ExcpEnvironmentCallFromUmode
-	ExcpEnvironmentCallFromSmode
-	ExcpEnvironmentCallFromMmode
-	ExcpInstructionPageFault
-	ExcpLoadPageFault
-	ExcpStoreAMOPageFault
+	// not using iota because code is not just a sequence number
+	ExcpNone                         Exception = -1
+	ExcpInstructionAddressMisalighed Exception = 0
+	ExcpInstructionAccessFault       Exception = 1
+	ExcpIllegalInstruction           Exception = 2
+	ExcpBreakpoint                   Exception = 3
+	ExcpLoadAddressMisaligned        Exception = 4
+	ExcpLoadAccessFault              Exception = 5
+	ExcpStoreAMOAddressMisaligned    Exception = 6
+	ExcpStoreAMOAccessFault          Exception = 7
+	ExcpEnvironmentCallFromUmode     Exception = 8
+	ExcpEnvironmentCallFromSmode     Exception = 9
+	ExcpEnvironmentCallFromMmode     Exception = 11
+	ExcpInstructionPageFault         Exception = 12
+	ExcpLoadPageFault                Exception = 13
+	ExcpStoreAMOPageFault            Exception = 15
 )
+
+func (e Exception) Code() int {
+	return int(e)
+}
